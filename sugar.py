059b407ea5d1fe8e38baa9c4bbaea24fce9d1c6b
Kajetan Mazur <kajetan@webownia.pl>
2017-04-18 18:29:49 +0200

    -exercises - square numbers

---
+++ sugar.py
@@ -0,0 +1,9 @@
+list_numbers = [1,2,3,4,5,6,7,8,9]
+list_numbers_squares = []
+
+for element in list_numbers:
+    list_numbers_squares.append(element*element)
+print(list_numbers_squares)
+
+list3 = [element**2 for element in list_numbers]
+print(list3)
