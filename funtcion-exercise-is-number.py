52ddea97f67bc55d3ace45d1ba4145a3b30ad743
Kajetan Mazur <kajetan@webownia.pl>
2017-04-20 19:56:08 +0200

    -exercises - functions - is number in range - checking numbers from list

--- funtcion-exercise-is-number.py
+++ funtcion-exercise-is-number.py
@@ -8,3 +8,8 @@ x = 23
 y = range(1,23)
 is_number_in_range(x,y)
 print(is_number_in_range(x,y))
+
+numbers = [23,45,67,89,232,345,4564]
+
+for num in numbers:
+    print(is_number_in_range(num,range(100)))
