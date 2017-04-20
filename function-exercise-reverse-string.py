3c101df08bf98154b664ccdb111e593572d12b74
Kajetan Mazur <kajetan@webownia.pl>
2017-04-20 18:45:25 +0200

    -exercises - functions - reverse string

---
+++ function-exercise-reverse-string.py
@@ -0,0 +1,14 @@
+def reverse_string(sentence):
+    return sentence[::-1]
+
+
+
+reversed_string = reverse_string("John have cat")
+
+print(reversed_string)
+
+def reversing_string():
+    sentece = input("Give me sentence: ")
+    return sentece[::-1]
+
+print(reversing_string())
