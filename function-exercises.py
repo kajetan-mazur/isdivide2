15c874dd90537946d3792317ba307893c2e9c430
Kajetan Mazur <kajetan@webownia.pl>
2017-04-20 18:03:52 +0200

    -exercises - functions

---
+++ function-exercises.py
@@ -0,0 +1,10 @@
+def print_squares(max_num):
+    for l in range(max_num):
+        print(l**2)
+
+print_squares(10)
+
+def print_squares(from_num, to_num):
+    for l in range(from_num, to_num):
+        print(l**2)
+print_squares(1,20)
