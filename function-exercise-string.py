170b14cb645fa09d52622d81f801718a6d35d54c
Kajetan Mazur <kajetan@webownia.pl>
2017-04-25 14:10:04 +0200

    -exercises - homework

---
+++ function-exercise-string.py
@@ -0,0 +1,15 @@
+from function-exercise-reverse-string import *
+
+x = reve
+
+import 
+# import string
+#
+# def is_panagram(keyword):
+#     for l in string.ascii_lowercase:
+#         if l not in str(keyword).lower():
+#             return False
+#     return True
+#
+# print(is_panagram("abcDE"))
+# print(is_panagram("The quick brown fox jumps over the lazy dog"))
---
+++ homework/homework-4.py
@@ -0,0 +1,39 @@
+# z = input('Give me the height of the pyramid: ');
+# n = int(z);
+#
+# for i in range(1, n+1):
+#     print(" " * (10 - i) + "#" * i + "#" * (i - 1))
+
+# number_even= 0;
+# number_odd=0;
+# n = int(input("Give me down range: "));
+# z = int(input("Give me up range: "));
+# for x in range(n,z):
+#     if x%2 == 0:
+#         number_even += 1
+#     if x%2 != 0:
+#         number_odd +=1
+#
+# print("How many even: " + str(number_even))
+# print("How many odd: " + str(number_odd))
+
+# for x in range(0,20):
+#     if x%4 != 0:
+#         print(x)
+
+# def fib(n):
+#     a,b = 1,1
+#     for i in range(n-1):
+#         a,b=b,a+b
+#     return a,b
+# print(fib(10))
+
+# import fibo
+# def fib2(n):
+#     result = []
+#     a, b = 0, 1
+#     while b < n:
+#         result.append(b)
+#         a, b = b, a+b
+#     return result
+# fibo.fib2(100)
